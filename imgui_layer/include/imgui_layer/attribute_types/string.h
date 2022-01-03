5baa1f0730498a99d53bb73d91548fb87cce7f54
Jannis <fluxxcodegames@gmail.com>
2022-01-03 15:42:48 +0100

    Add attribute types to string conversion
    Added conversion for every attribute type to string.

--- imgui_layer/include/imgui_layer/attribute_types/string.h
+++ imgui_layer/include/imgui_layer/attribute_types/string.h
@@ -1,6 +1,7 @@
 #ifndef IMGUI_LAYER_SRC_ATTRIBUTE_TYPES_STRING_H_
 #define IMGUI_LAYER_SRC_ATTRIBUTE_TYPES_STRING_H_
 
+// Also includes forward declarions for other attribute types
 #include "imgui_layer/attribute_types/attribute_type.h"
 
 #include <string>
@@ -23,6 +24,12 @@ public:
     inline operator const char*() const { return value.c_str(); }
 
 private:
+    bool IMPL_LoadValue(const Bool& value);
+    bool IMPL_LoadValue(const Float& value);
+    bool IMPL_LoadValue(const Float2& value);
+    bool IMPL_LoadValue(const Float3& value);
+    bool IMPL_LoadValue(const Float4& value);
+    bool IMPL_LoadValue(const Int& value);
     bool IMPL_LoadValue(const String& value);
 };
 
