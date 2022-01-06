715d52038b5c8f285db6b7859b9c92ecd285c969
Jannis <fluxxcodegames@gmail.com>
2022-01-06 23:44:12 +0100

    Add missing object includes

--- imgui_layer/include/imgui_layer.h
+++ imgui_layer/include/imgui_layer.h
@@ -27,8 +27,17 @@
 #include "imgui_layer/objects/global_object.h"
 #include "imgui_layer/objects/text.h"
 #include "imgui_layer/objects/button.h"
+#include "imgui_layer/objects/button_style.h"
 #include "imgui_layer/objects/panel.h"
 #include "imgui_layer/objects/child_panel.h"
+#include "imgui_layer/objects/container.h"
+#include "imgui_layer/objects/attribute_types/object_bool.h"
+#include "imgui_layer/objects/attribute_types/object_float.h"
+#include "imgui_layer/objects/attribute_types/object_float2.h"
+#include "imgui_layer/objects/attribute_types/object_float3.h"
+#include "imgui_layer/objects/attribute_types/object_float4.h"
+#include "imgui_layer/objects/attribute_types/object_int.h"
+#include "imgui_layer/objects/attribute_types/object_string.h"
 
 #include "imgui_layer/common_functions.h"
 
