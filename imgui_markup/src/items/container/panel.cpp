033feddb7bc8a32f514543a2fbcc5b89b4098c99
Jannis <info.fluxxcode@gmail.com>
2022-03-13 19:25:18 +0100

    Move WindowFlags to Panel
    Deleted the seperate WindowFlags and moved them to the Panel.

--- imgui_markup/src/items/container/panel.cpp
+++ imgui_markup/src/items/container/panel.cpp
@@ -10,12 +10,28 @@ namespace imgui_markup::internal
 Panel::Panel(std::string id, ItemBase* parent)
     : ContainerBase(ItemType::kPanel, id, parent)
 {
-    this->InitWindowFlagAttributes(this->attribute_list_);
-
     this->AddAttribute("title",    &this->title_);
     this->AddAttribute("position", &this->position_overwrite_);
     this->AddAttribute("size",     &this->size_overwrite_);
     this->AddAttribute("padding",  &this->padding_);
+
+    // Flags:
+    this->AddAttribute("no_title_bar",          &this->no_title_bar_);
+    this->AddAttribute("no_resize",             &this->no_resize_);
+    this->AddAttribute("no_move",               &this->no_move_);
+    this->AddAttribute("no_collapse",           &this->no_collapse_);
+    this->AddAttribute("always_auto_resize",    &this->always_auto_resize_);
+    this->AddAttribute("no_background",         &this->no_background_);
+    this->AddAttribute("no_mouse_inputs",       &this->no_mouse_inputs_);
+    this->AddAttribute("no_focus_on_appearing", &this->no_focus_on_appearing_);
+    this->AddAttribute("no_nav_inputs",         &this->no_nav_inputs_);
+    this->AddAttribute("no_nav_focus",          &this->no_nav_focus_);
+    this->AddAttribute("unsaved_document",      &this->unsaved_document_);
+    this->AddAttribute("no_nav",                &this->no_nav_);
+    this->AddAttribute("no_decoration",         &this->no_decoration_);
+    this->AddAttribute("no_inputs",             &this->no_inputs_);
+    this->AddAttribute("no_bring_to_front_focus",
+        &this->no_bring_to_front_focus_);
 }
 
 void Panel::IMPL_Update(Float2 position, Float2 size)
@@ -103,6 +119,54 @@ float Panel::CalcTitleBarHeight() const
     ImGuiStyle& style = ImGui::GetStyle();
 
     return ImGui::GetFontSize() + style.FramePadding.y * 2.0f;
+}
+
+int Panel::GenerateWindowFlags() const
+{
+    ImGuiWindowFlags flags = ImGuiWindowFlags_None;
+
+    if (this->no_title_bar_)
+        flags |= ImGuiWindowFlags_NoTitleBar;
+    if (this->no_resize_)
+        flags |= ImGuiWindowFlags_NoResize;
+    if (this->no_move_)
+        flags |= ImGuiWindowFlags_NoMove;
+    if (this->no_scrollbar_)
+        flags |= ImGuiWindowFlags_NoScrollbar;
+    if (this->no_scroll_with_mouse_)
+        flags |= ImGuiWindowFlags_NoScrollWithMouse;
+    if (this->no_collapse_)
+        flags |= ImGuiWindowFlags_NoCollapse;
+    if (this->always_auto_resize_)
+        flags |= ImGuiWindowFlags_AlwaysAutoResize;
+    if (this->no_background_)
+        flags |= ImGuiWindowFlags_NoBackground;
+    if (this->no_mouse_inputs_)
+        flags |= ImGuiWindowFlags_NoMouseInputs;
+    if (this->horizontal_scrollbar_)
+        flags |= ImGuiWindowFlags_HorizontalScrollbar;
+    if (this->no_focus_on_appearing_)
+        flags |= ImGuiWindowFlags_NoFocusOnAppearing;
+    if (this->no_bring_to_front_focus_)
+        flags |= ImGuiWindowFlags_NoBringToFrontOnFocus;
+    if (this->always_vertical_scrollbar_)
+        flags |= ImGuiWindowFlags_AlwaysVerticalScrollbar;
+    if (this->always_horizontal_scrollbar_)
+        flags |= ImGuiWindowFlags_AlwaysHorizontalScrollbar;
+    if (this->no_nav_inputs_)
+        flags |= ImGuiWindowFlags_NoNavInputs;
+    if (this->no_nav_focus_)
+        flags |= ImGuiWindowFlags_NoNavFocus;
+    if (this->unsaved_document_)
+        flags |= ImGuiWindowFlags_UnsavedDocument;
+    if (this->no_nav_)
+        flags |= ImGuiWindowFlags_NoNav;
+    if (this->no_decoration_)
+        flags |= ImGuiWindowFlags_NoDecoration;
+    if (this->no_inputs_)
+        flags |= ImGuiWindowFlags_NoInputs;
+
+    return flags;
 }
 
 bool Panel::OnProcessStart(std::string& error_message)
--- imgui_markup/src/items/container/panel.h
+++ imgui_markup/src/items/container/panel.h
@@ -2,7 +2,6 @@
 #define IMGUI_MARKUP_SRC_ITEMS_CONTAINER_PANEL_H_
 
 #include "items/common/container_base.h"
-#include "items/common/window_flags.h"
 
 #include "attribute_types/bool.h"
 #include "attribute_types/string.h"
@@ -13,7 +12,7 @@ namespace imgui_markup::internal
 
 class PanelStyle;
 
-class Panel : public ContainerBase, public WindowFlags
+class Panel : public ContainerBase
 {
 public:
     // Constructor/Destructor
@@ -24,6 +23,35 @@ public:
     Float2 size_overwrite_;
     String title_;
     Float2 padding_ = Float2(0.0f, 0.0f);
+
+    // Flags:
+    Bool no_title_bar_             = false;
+    Bool no_resize_                = false;
+    Bool no_move_                  = false;
+    Bool no_collapse_              = false;
+    Bool always_auto_resize_       = false;
+    Bool no_background_            = false;
+    Bool no_mouse_inputs_          = false;
+    Bool no_focus_on_appearing_    = false;
+    Bool no_bring_to_front_focus_  = false;
+    Bool no_nav_inputs_            = false;
+    Bool no_nav_focus_             = false;
+    Bool unsaved_document_         = false;
+    Bool no_nav_                   = false;
+    Bool no_decoration_            = false;
+    Bool no_inputs_                = false;
+
+    // Flags that can't be set through the markup language
+    Bool no_scroll_with_mouse_ = true;
+    Bool no_scrollbar_         = true;
+    Bool horizontal_scrollbar_ = false;
+    Bool always_vertical_scrollbar_   = false;
+    Bool always_horizontal_scrollbar_ = false;
+
+    // Flags we don't need but ImGui supports
+    // Bool always_use_window_padding_ = false;
+    // Bool menu_bar_ = false;
+    // Bool no_saved_settings_ = false;
 
 private:
     friend class PanelStyle;
@@ -39,6 +67,7 @@ private:
     void InitPositionAndSize(Float2 position, Float2 size);
 
     float CalcTitleBarHeight() const;
+    int GenerateWindowFlags() const;
 
     // See item.h for documentation
     void IMPL_Update(Float2 position, Float2 size);
